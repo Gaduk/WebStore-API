b56039aa11d082b80f125f98928704ff858669a1
Gaduk <dana.kozenko@ya.ru>
2024-08-10 18:23:44 +0300

    first commit

---
+++ Web-API/Database/Good.cs
@@ -0,0 +1,8 @@
+namespace Web_API;
+
+public class Order
+{
+    public string Login { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
---
+++ Web-API/Database/User.cs
@@ -0,0 +1,6 @@
+namespace Web_API;
+
+public class User
+{
+    
+}

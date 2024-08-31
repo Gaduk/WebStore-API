bb962ed0414bb675477a18b3c889f1bf397ba6fd
Gaduk <dana.kozenko@ya.ru>
2024-08-31 19:52:45 +0300

    added extension to IHost

--- API/Program.cs
+++ API/Program.cs
@@ -1,13 +1,16 @@
+using Persistence.Context;
+using Web_API.Extensions;
+
 namespace Web_API;
 
 public class Program
 {
     public static void Main(string[] args)
     {
-        CreateHostBuilder(args).Build().Run();
+        CreateHostBuilder(args).Build().MigrateDatabase<ApplicationDbContext>().Run();
     }
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
+    private static IHostBuilder CreateHostBuilder(string[] args) =>
         Host.CreateDefaultBuilder(args)
             .ConfigureWebHostDefaults(webBuilder =>
             {

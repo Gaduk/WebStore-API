82864da0ae73fffa256547981d27c9da7a9b9568
Gaduk <dana.kozenko@ya.ru>
2024-09-09 13:48:15 +0300

    added admin phone number migration

---
+++ Persistence/Migrations/20240909102606_added_admin_phone_number.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class added_admin_phone_number : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "admin",
+                column: "PhoneNumber",
+                value: "+79001113344");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "admin",
+                column: "PhoneNumber",
+                value: null);
+        }
+    }
+}

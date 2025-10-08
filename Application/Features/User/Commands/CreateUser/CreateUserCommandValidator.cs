fa09a38093e2e3847b1542d27eef5caf456e9dad
Gaduk <dana.kozenko@ya.ru>
2025-10-08 16:19:01 +0300

    fixed incorrect validation message

--- Application/Features/User/Commands/CreateUser/CreateUserCommandValidator.cs
+++ Application/Features/User/Commands/CreateUser/CreateUserCommandValidator.cs
@@ -12,7 +12,7 @@ public class CreateUserCommandValidator : AbstractValidator<CreateUserCommand>
 
         RuleFor(x => x.Password)
             .NotEmpty().WithMessage("Password is required")
-            .MinimumLength(3).WithMessage("Password must be at least 6 characters long")
+            .MinimumLength(3).WithMessage("Password must be at least 3 characters long")
             .MaximumLength(30).WithMessage("Password must not exceed 30 characters");
 
         RuleFor(x => x.FirstName)

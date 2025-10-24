3edcc70fc332e22859d377fc28667dc6dd64aa5b
Gaduk <dana.kozenko@ya.ru>
2025-10-24 15:33:57 +0300

    added image to Good entity

--- Domain/Entities/Good.cs
+++ Domain/Entities/Good.cs
@@ -2,7 +2,8 @@ namespace Domain.Entities;
 
 public class Good
 {
-    public int Id { get; init; }
-    public string? Name { get; init; }
-    public int Price { get; init; }
+    public int     Id    { get; init; }
+    public string? Name  { get; set ; }
+    public int     Price { get; set ; }
+    public byte[]? Image { get; set ; }
 }

2deb0a1e743de43f358f975307337d06303ceed8
Gaduk <dana.kozenko@ya.ru>
2025-10-27 15:14:58 +0300

    removed image from Good entity

--- Domain/Entities/Good.cs
+++ Domain/Entities/Good.cs
@@ -5,5 +5,4 @@ public class Good
     public int     Id    { get; init; }
     public string? Name  { get; set ; }
     public int     Price { get; set ; }
-    public byte[]? Image { get; set ; }
 }

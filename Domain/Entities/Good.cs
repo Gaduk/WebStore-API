421c108f5844772b5826ba38bedb8ae03106bee2
Gaduk <dana.kozenko@ya.ru>
2025-10-28 15:13:27 +0300

    added imageURLs to Good entity

--- Domain/Entities/Good.cs
+++ Domain/Entities/Good.cs
@@ -2,7 +2,8 @@ namespace Domain.Entities;
 
 public class Good
 {
-    public int     Id    { get; init; }
-    public string? Name  { get; set ; }
-    public int     Price { get; set ; }
+    public int          Id        { get; init; }
+    public string?      Name      { get; set ; }
+    public int          Price     { get; set ; }
+    public List<string> ImageUrls { get; set ; } = [];
 }

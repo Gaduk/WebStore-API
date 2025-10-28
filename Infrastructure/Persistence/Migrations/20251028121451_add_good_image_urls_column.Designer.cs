3f570527c1c868eb59943f16de7dc1c5f7d3fe4b
Gaduk <dana.kozenko@ya.ru>
2025-10-28 15:22:32 +0300

    db migrated

--- Infrastructure/Persistence/Migrations/20251028121451_add_good_image_urls_column.Designer.cs
+++ Infrastructure/Persistence/Migrations/20251028121451_add_good_image_urls_column.Designer.cs
@@ -4,17 +4,20 @@ using System.Collections.Generic;
 using Infrastructure.Persistence.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 #nullable disable
 
-namespace Infrastructure.Migrations
+namespace Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251028121451_add_good_image_urls_column")]
+    partial class add_good_image_urls_column
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Infrastructure/Persistence/Migrations/20251028121451_add_good_image_urls_column.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_good_image_urls_column : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<List<string>>(
+                name: "ImageUrls",
+                table: "Goods",
+                type: "text[]",
+                nullable: false,
+                defaultValue: new List<string>());
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrls",
+                table: "Goods");
+        }
+    }
+}

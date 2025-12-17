33be8c34126a37708ce05ffa589f273376054883
Gaduk <dana.kozenko@ya.ru>
2025-12-17 16:59:27 +0300

    removed id field from BasketItem entity

--- Domain/Entities/BasketItem.cs
+++ Domain/Entities/BasketItem.cs
@@ -2,10 +2,9 @@ namespace Domain.Entities;
 
 public class BasketItem
 {
-    public Guid   Id       { get; init; } = Guid.NewGuid();
     public string UserName { get; init; } = "";
     public int    GoodId   { get; init; }
     public int    Amount   { get; set;  }
 
-    public Good?  Good     { get; init; }
+    public Good   Good     { get; init; } = null!;
 }

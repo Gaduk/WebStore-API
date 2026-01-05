9bfb6d7bec3392fa26689ad29ff122fd4a47057e
Gaduk <dana.kozenko@ya.ru>
2026-01-05 21:18:40 +0300

    migrated db (updated basket items primary key)

---
+++ Infrastructure/Persistence/Migrations/20260105142832_update_basket_items_primary_key.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class update_basket_items_primary_key : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_BasketItems",
+                table: "BasketItems");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "BasketItems");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_BasketItems",
+                table: "BasketItems",
+                columns: new[] { "UserName", "GoodId" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_BasketItems",
+                table: "BasketItems");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "Id",
+                table: "BasketItems",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_BasketItems",
+                table: "BasketItems",
+                column: "Id");
+        }
+    }
+}
